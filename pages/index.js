7a051e55831d21787337a106a94b4c18ad4f704c
Kaiwen Du <TheKaiwenDu@gmail.com>
2024-11-16 19:49:02 -0800

    Commit Index

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import Image from "next/image";
 import localFont from "next/font/local";
 
 import Header from "@/components/Header";
-
+import Title from "@/components/Title";
+import CardInformation from "@/components/CardInformation";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-      <div className={"h-screen"}>
-        <Header currentPage={'home'}/>
-      </div>
+	<div style={{ backgroundColor: '#534d42', color: 'white'}}>
+		<Header currentPage={'home'}/>
+		<Title/>
+		<SearchBar/>
+		<CardInformation/>
+	</div>
   );
 }
